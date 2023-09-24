aeb2e36a8dd9d5b7eb3ffe27ca83103053aa9209
Филиппов Фёдор <131267413+Dungier@users.noreply.github.com>
2023-09-24 18:22:55 +0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
               className="block max-w-[40rem] max-h-[42rem] rounded-lg bg-white text-center dark:bg-neutral-700 mt-5">
               <div className="p-6">
                   <h5
-                      className="mb-1 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                      className="mb-1 text-xs font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                       {"Привет, вы на главной странице тестового задания, для просмотра выполнение: перейдите на вкладку PostsPage или используя роут /posts_page, так же вы можете просто кликнуть на котика)"}
                   </h5>
                   <p
                       className="mb-4 mt-6">
                       <Link href={`/posts_page`}>
-                          <img style={{borderRadius: "15px", width: "15rem", height: "15rem"}} src='https://sun9-1.userapi.com/impg/z0qxolF6x-NcVxZ_bFyZmFVt_iw0gQhrYohhGw/EmjyKnPAW-Y.jpg?size=736x808&quality=96&sign=af025c0e71634b65ee7caac2262b1f4d&type=album' alt='img'/>
+                          <img className="max-w-[20rem] max-h-[20rem]" style={{borderRadius: "15px"}} src='https://sun9-1.userapi.com/impg/z0qxolF6x-NcVxZ_bFyZmFVt_iw0gQhrYohhGw/EmjyKnPAW-Y.jpg?size=736x808&quality=96&sign=af025c0e71634b65ee7caac2262b1f4d&type=album' alt='img'/>
                       </Link>
                   </p>
               </div>
